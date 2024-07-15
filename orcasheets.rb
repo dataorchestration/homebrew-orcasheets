dfb8835111d24776c373cd6634387c91187dc46b
navdeep710 <navdeep710@gmail.com>
2024-07-16 02:58:38 +0530

    fix script

--- orcasheets.rb
+++ orcasheets.rb
@@ -12,8 +12,10 @@ class Orcasheets < Formula
   end
 
   def install
+    # print the current directory
+    puts Dir.pwd
     # Since orcasheets.app is at the root of the tarball, install it directly
-    prefix.install "orcasheets.app"
+    prefix.install Dir["orcasheets.app"]
   end
 
   def caveats
