55b71958e982d173e69600859e3011218c320626
navdeep710 <navdeep710@gmail.com>
2024-07-17 00:02:23 +0530

    fixed sha

--- Casks/orcasheets.rb
+++ Casks/orcasheets.rb
@@ -2,7 +2,7 @@ cask "orcasheets" do
   arch arm: "aarch64", intel: "x64"
 
   version "2024.7.16"
-  sha256 arm:   "17751340a0f29544e40fc9c4347a179a4e42bbb435c1fb8bc67f485a668188ed",
+  sha256 arm:   "2e14af595c5da463d7ae8777aa1c9411f6cf93c67608c080b2f15eb572a96224",
          intel: "91157027ffe8164866fd2a09f3784cfe3b1414ce1537d4536c9a4cf6a17937ed"
 
   url "https://github.com/dataorchestration/homebrew-orcasheets/releases/download/#{version}/orcasheets_#{version}_#{arch}.dmg",
