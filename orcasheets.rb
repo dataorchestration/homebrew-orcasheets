a37a19967859d22d82d91b835c860a016cde3583
Mayur <mj.jadhav13@gmail.com>
2024-07-16 09:31:35 +0530

    Update orcasheets.rb

--- orcasheets.rb
+++ orcasheets.rb
@@ -5,7 +5,7 @@ class Orcasheets < Formula
 
   if Hardware::CPU.intel?
     url "https://github.com/dataorchestration/homebrew-orcasheets/releases/download/v0.0.2/orcasheets-intel.tar.gz"
-    sha256 "<INTEL_SHA256_CHECKSUM>"
+    sha256 "0ca09c9ebf32931f476440b0c56d94e5cfc718fae99566ccdce7bd5df1b713f1"
   elsif Hardware::CPU.arm?
     url "https://github.com/dataorchestration/homebrew-orcasheets/releases/download/v0.0.2/orcasheets-arm.tar.gz"
     sha256 "9218cba4d78941ed7ae1f39a824384ee5d8730a64f0e344d1e46bfbba2b2e6a9"
