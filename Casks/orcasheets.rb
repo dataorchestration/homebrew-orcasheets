1cea378d56a43ce20a5b6122b5b1917bbfee44a1
navdeep710 <navdeep710@gmail.com>
2024-07-16 23:00:26 +0530

    added dmg verified build

--- Casks/orcasheets.rb
+++ Casks/orcasheets.rb
@@ -2,7 +2,7 @@ cask "orcasheets" do
   arch arm: "aarch64", intel: "x64"
 
   version "2024.7.16"
-  sha256 arm:   "73b7b27999a729087ac13d4664771856311d2b5e71ef262fd73c859f5627d446",
+  sha256 arm:   "17751340a0f29544e40fc9c4347a179a4e42bbb435c1fb8bc67f485a668188ed",
          intel: "91157027ffe8164866fd2a09f3784cfe3b1414ce1537d4536c9a4cf6a17937ed"
 
   url "https://github.com/dataorchestration/homebrew-orcasheets/releases/download/#{version}/orcasheets_#{version}_#{arch}.dmg",
