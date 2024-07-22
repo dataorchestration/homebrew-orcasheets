df6ecd0d69f09394e576e4831fbe8d5ccff5af9c
navdeep710 <navdeep710@gmail.com>
2024-07-22 19:16:32 +0530

    added new sha

--- Casks/orcasheets.rb
+++ Casks/orcasheets.rb
@@ -1,9 +1,9 @@
 cask "orcasheets" do
-  arch arm: "aarch64", intel: "x64"
+  arch arm: "arm64", intel: "x86_64"
 
   version "2024.7.16"
-  sha256 arm:   "780cd2c4f687f7192e7843cd0936e905a7f6d9f89498e2f7f913705919eaba8d",
-         intel: "44d048fc9e5f109151d8c6fa8fc85b2bc2245a8a7a2bb1e8d5bf597aef937476"
+  sha256 arm:   "41c1e1a15c1178fce5e97029a10007d15ff6f8e0449e2c9ca229fd95b877ae74",
+         intel: "2f039bae1d64095011acf57fcb176576ee21a98c73242edfdca4546fcfc438da"
 
   url "https://github.com/dataorchestration/homebrew-orcasheets/releases/download/#{version}/orcasheets_#{version}_#{arch}.dmg",
       verified: "github.com/dataorchestration/orcasheets/"
