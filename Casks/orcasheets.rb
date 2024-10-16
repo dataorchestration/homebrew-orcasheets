a0794522a5db64f09b613a780aaafa7e9ebb8512
navdeep710 <navdeep710@gmail.com>
2024-10-16 23:43:52 +0530

    Update OrcaSheets Cask file for version

--- Casks/orcasheets.rb
+++ Casks/orcasheets.rb
@@ -1,9 +1,9 @@
 cask "orcasheets" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "2024.8.9"
-  sha256 arm:   "6def544bd8a97872131c9923a7178fb7a9380826e2c7d79b14334eaf45dd9051",
-         intel: "e3de1be847de7061c8f167c28640933020a2a02873fb5f2fc947c7152d5985e9"
+  version "2024.10.1"
+  sha256 arm:   "a75f62c2a54e93e5c724a32d1622610e2272989ba874855177cab7d7cdb9b35a",
+         intel: "8e795090da9647ca56bf1f1012078d8e8b1c42057dca8bb7df2a05e7a8d07d48"
 
   url "https://github.com/dataorchestration/homebrew-orcasheets/releases/download/#{version}/orcasheets_#{version}_#{arch}.dmg",
       verified: "github.com/dataorchestration/homebrew-orcasheets/"
