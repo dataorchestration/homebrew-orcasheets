2c7b42059871627f36ce119d8a629b63daada407
Mayur Jadhav <mj.jadhav13@gmail.com>
2026-04-25 12:27:52 +0530

    Update OrcaSheets Cask file and metadata for version 26.4.2

--- Casks/orcasheets.rb
+++ Casks/orcasheets.rb
@@ -1,9 +1,9 @@
 cask "orcasheets" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "26.4.1"
-  sha256 arm:   "70eab6e9837e0eeb27e48be5eac6e70bdc60ca8450b065aa74d5d1bd99e67460",
-         intel: "2a0da40f31463409f20e9e6eeae5ace4ef94331397828fa93f36e9d0b83d5c32"
+  version "26.4.2"
+  sha256 arm:   "2fec4a6ec8069ed9f9998c1d71143194dafb3d5d56592cc3c7da42979abbc299",
+         intel: "2712aac8a600c29cd0081bae3740b66cca3e7d4d65954a4db1a80c1957f3e6e2"
 
   url "https://github.com/dataorchestration/homebrew-orcasheets/releases/download/#{version}/orcasheets_#{version}_#{arch}.dmg",
       verified: "github.com/dataorchestration/homebrew-orcasheets/"
